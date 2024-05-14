edfbd59e2e5535eeb8f42d74bcf263070aaae68e
rubenMkda <moncadaruben22@gmail.com>
2024-05-14 16:03:43 -0400

    elimando bg canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import MainHome from "@/components/main";
-import UseCustomCursor from "@/components/poinert";
-import CanvasBackground from "@/components/poinert";
-import CursorTracker from "@/components/poinert";
 import Proyectos from "@/components/proyectos";
-import Image from "next/image";
-import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
     <>
     <main id="wrapper">
-      <CanvasBackground />
        <MainHome />
        <Proyectos />
     </main>
